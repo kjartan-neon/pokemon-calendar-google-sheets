2db17c7ed16bcb72b97e0ec42e94c4066b5bf04c
Kjartan Bjørndal Michalsen <kjartan@liksom.no>
2025-10-23 12:02:38 +0200

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -400,7 +400,7 @@
 
   .app-title {
     font-family: var(--font-retro);
-    font-size: var(--font-size-3xl);
+    font-size: var(--font-size-l);
     font-weight: var(--font-weight-bold);
     color: white;
     margin: 0;
